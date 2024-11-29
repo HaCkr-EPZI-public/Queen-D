5fbdf1309067244c1667c01b2c0b0282326815c0
Asᴍᴏᴅᴇᴜs Eᴘᴢɪ 💸 <99525582+HaCkr-EPZI-public@users.noreply.github.com>
2024-11-29 15:40:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/* Copyright (C) 2024 Asmodeus Epzi.
+Licensed under the  MIT License;
+you may not use this file except in compliance with the License.
+Queen D ~ ESI Devs ~ Asmodeus Epzi
+-------------------------------------------------------
+re-upload? recode? copy code? give credit ya :)
+YouTube: @EPZi
+instagram: yuren.sasanka
+Telegram: t.me/asmodeus_epzi
+GitHub: @HaCkr-EPZI-public
+WhatsApp: +94759554531
+*/
+
 const fs = require("fs").promises;
 const fsx = require("fs");
 const path = require("path");
